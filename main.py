32db8fef71ecf3b689538ba4914816b99eaec927
root <root@Maschina.localdomain>
2025-02-01 17:20:20 +0100

    Organize into functions.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import sys
+import json
 from todoist_api_python.api_async import TodoistAPIAsync
 from todoist_api_python.api import TodoistAPI
 
-print(sys.argv)
 
-token = sys.argv[1]
+def get_projects(api):
+    try:
+        projects = api.get_projects()
+        return projects
+    except Exception as error:
+        print(error)
 
-api = TodoistAPI(token)
 
-try:
-    projects = api.get_projects()
+def main():    
+    token = sys.argv[1]
+    api = TodoistAPI(token)
+    projects = get_projects(api)
     print(projects)
-except Exception as error:
-    print(error)
+
+
+if __name__ == '__main__':
+    main()
